f390b6508e9b6c094142731f867f604c87b5ebc2
abank <banka@rpi.edu>
2023-01-09 21:44:43 -0500

    I hope I didn't break everything

--- src/First/Headquarters.java
+++ src/First/Headquarters.java
@@ -1,8 +1,22 @@
 package First;
 
 import battlecode.common.*;
-
+import java.util.Random;
 public class Headquarters extends Robot {
+  //stuff I stole from examplefuncsplayer
+  static final Direction[] directions = {
+    Direction.NORTH,
+    Direction.NORTHEAST,
+    Direction.EAST,
+    Direction.SOUTHEAST,
+    Direction.SOUTH,
+    Direction.SOUTHWEST,
+    Direction.WEST,
+    Direction.NORTHWEST,
+  };
+  static final Random rng = new Random(6147);
+
+
   public void run(RobotController rc) throws GameActionException {
     //todo: anchors
     // Pick a direction to build in.
