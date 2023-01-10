3792b3c96c854868a24287cc0853c7128f75acc8
abank <banka@rpi.edu>
2023-01-09 21:36:39 -0500

    pls work

--- src/First/Headquarters.java
+++ src/First/Headquarters.java
@@ -4,6 +4,14 @@ import battlecode.common.*;
 
 public class Headquarters extends Robot {
   public void run(RobotController rc) throws GameActionException {
-
+    //todo: anchors
+    // Pick a direction to build in.
+    Direction dir = directions[rng.nextInt(directions.length)];
+    MapLocation newLoc = rc.getLocation().add(dir);
+    // Let's try to build a carrier.
+    rc.setIndicatorString("Trying to build a carrier");
+    if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+        rc.buildRobot(RobotType.CARRIER, newLoc);
+    }
   }
 }
