b84fd8b9530b5945784f99fc43f2a414d92cc624
justinottesen <justinottesen@gmail.com>
2023-01-11 11:59:37 -0500

    HQ now has select conditions for spawning carriers, this is temporary fix, doesn't account for carriers dying

--- src/First/Headquarters.java
+++ src/First/Headquarters.java
@@ -3,6 +3,11 @@ package First;
 import battlecode.common.*;
 import java.util.Random;
 public class Headquarters extends Robot {
+
+  // Keeps track of how many we have made
+  private static int num_Carriers = 0;
+  private static int num_Launcehrs = 0;
+
   //stuff I stole from examplefuncsplayer
   static final Direction[] directions = {
     Direction.NORTH,
@@ -32,13 +37,10 @@ public class Headquarters extends Robot {
     MapLocation newLoc = rc.getLocation().add(dir);
     // Let's try to build a carrier.
     rc.setIndicatorString("Trying to build a carrier");
-    if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+    if (rc.canBuildRobot(RobotType.CARRIER, newLoc) && (num_Carriers < 5 || num_Carriers < 1.5*num_Launcehrs)) {
       rc.buildRobot(RobotType.CARRIER, newLoc);
     }else if(rc.canBuildRobot(RobotType.LAUNCHER,newLoc)){
       rc.buildRobot(RobotType.LAUNCHER, newLoc);
-    }
-    for(int i=0;i<100;++i){
-      MapLocation m=new MapLocation(i,i);
     }
   }
 }
