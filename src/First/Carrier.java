4df4c7f9d75d2db72c361e05c45c01e65f024410
abank <banka@rpi.edu>
2023-01-11 13:42:57 -0500

    created elixir carriers and fixed rerouting

--- src/First/Carrier.java
+++ src/First/Carrier.java
@@ -3,15 +3,21 @@ package First;
 import battlecode.common.*;
 
 public class Carrier extends Robot {
-
   private static boolean well_found;
   private static WellInfo[] nearby_wells;
   private static WellInfo goal;
   private static MapLocation goal_loc;
+  private static boolean elixir;
+  private static WellInfo goal2;
+  private static MapLocation goal_loc2;
 
   public Carrier(RobotController rc) throws GameActionException {
     super(rc);
     well_found = false;
+    //elixir code below
+    elixir=false;
+    goal2=null;
+    goal_loc2=null;
   }
 
   public void run(RobotController rc) throws GameActionException {
@@ -19,19 +25,60 @@ public class Carrier extends Robot {
     
     if (!well_found) {
       nearby_wells = rc.senseNearbyWells();
-      well_found = (nearby_wells.length > 0) ? true : false;
+      int i;
+      for(i=0;i<nearby_wells.length;++i){//picks a well that isn't crowded
+        if(rc.senseNearbyRobots(nearby_wells[i].getMapLocation(),6,rc.getTeam()).length<4){
+          well_found=true;
+          break;
+        }
+      }
       if (well_found) {
-        goal = getClosest(rc, nearby_wells);
+        goal = nearby_wells[i];
         goal_loc = goal.getMapLocation();
+        //elixir code below
+        if(goal.getResourceType()!=ResourceType.ELIXIR&&rc.getRoundNum()%10==0){//one in 10 chance for carrier to be an elixir carrier
+          elixir=true;
+          rc.setIndicatorString("elixir=true");
+        }
+      }
+    }else if(goal2==null&&elixir){//get second well for elixir
+      nearby_wells = rc.senseNearbyWells();
+      for(int i=0;i<nearby_wells.length;++i){
+        if(nearby_wells[i].getResourceType()!=ResourceType.ELIXIR&&nearby_wells[i].getResourceType()!=goal.getResourceType()){
+          goal2=nearby_wells[i];
+          goal_loc2=goal2.getMapLocation();
+        }
       }
     }
     
     Direction go;
-    if (well_found) {
-      // If it sees too many carriers, it moves away from them and finds a new well
-      if (rc.senseNearbyRobots(10, rc.getTeam()).length > 5 && rc.getResourceAmount(goal.getResourceType()) < 0.5*GameConstants.CARRIER_CAPACITY) {
-        go = goal_loc.directionTo(hqInfo.location).rotateLeft().rotateLeft();
-        well_found = false;
+    if(goal2!=null&&elixir){//make elixir well
+      rc.setIndicatorString("I'm an elixir carrier");
+      if (rc.getResourceAmount(goal.getResourceType()) < GameConstants.CARRIER_CAPACITY) { //collect from goal1
+        if (rc.getLocation().isAdjacentTo(goal_loc) && rc.canCollectResource(goal_loc, -1)) {
+          rc.collectResource(goal_loc, -1);
+        }
+        go = pathfind(rc, goal_loc);
+      } else { // COLLECTED 100% RESOURCES, dump off at goal2 well
+        if (rc.getLocation().isAdjacentTo(goal_loc2) && rc.canTransferResource(goal_loc2, goal.getResourceType(), rc.getResourceAmount(goal.getResourceType()))) {
+          rc.transferResource(goal_loc2, goal.getResourceType(), rc.getResourceAmount(goal.getResourceType()));
+        }
+        go = pathfind(rc, goal_loc2);
+      }
+      //check to see if well converted
+      //stop once the carrier has finished making the elixir well
+      if(rc.canSenseLocation(goal_loc2)){
+        goal2=rc.senseWell(goal_loc2);
+        if(goal2.getResourceType()==ResourceType.ELIXIR){//elixir converted, reset carrier to default
+          goal2=null;
+          goal_loc2=null;
+          elixir=false;
+          rc.setIndicatorString("I finished my elixir job!");
+        }
+      }
+    }else if (well_found) {
+      if(rc.canSenseLocation(goal_loc)){//update well info (in case the well is now an elixir well)
+        goal=rc.senseWell(goal_loc);
       }
       if (rc.getResourceAmount(goal.getResourceType()) < 0.5*GameConstants.CARRIER_CAPACITY) {
         if (rc.getLocation().isAdjacentTo(goal_loc) && rc.canCollectResource(goal_loc, -1)) {
